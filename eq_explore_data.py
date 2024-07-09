58014cac4ddfda71043cd92558b96187635f14ea
lukwac123 <lukasz-waclawek@wp.pl>
2024-07-09 22:27:11 +0200

    Wyodrębnienie danych o sile trzęsień ziemi.

--- eq_explore_data.py
+++ eq_explore_data.py
@@ -10,3 +10,15 @@ all_eq_data = json.loads(contents)
 path = Path('eq_data/readable_eq_data.geojson')
 readable_contents = json.dumps(all_eq_data, indent=4)
 path.write_text(readable_contents)
+
+# Analiza wszystkich trzęsień na podstawie zbioru danych.
+all_eq_data = all_eq_data['features']
+# Poniższy wiersz wyświetla liczbę trzęsień ziemi.
+# print(len(all_eq_data))
+
+mags = []
+for eq_dict in all_eq_data:
+    mag = eq_dict['properties']['mag']
+    mags.append(mag)
+# Poniżej sprawdzenie czy mamy poprawne dane (pierwszych 10 rekordów).
+# print(mags[:10])
