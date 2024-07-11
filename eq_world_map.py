1820de3608d3fa743c631a2456ba928568929df8
lukwac123 <lukasz-waclawek@wp.pl>
2024-07-11 17:40:37 +0200

    Dodanie siły trzęsienia ziemi, zmiana skali kolorów. Zmiana wzoru wyświetlaniej mapy.

--- eq_world_map.py
+++ eq_world_map.py
@@ -4,13 +4,14 @@ import json
 import plotly.express as px
 
 # Odczytanie danych jako ciągu tekstowego i ich konwersja na obiekt Pythona.
-path = Path('eq_data/eq_data_1_day_m1.geojson')
+path = Path('eq_data/eq_data_30_day_m1.geojson')
 contents = path.read_text(encoding='utf-8')
 all_eq_data = json.loads(contents)
 
 # Analiza wszystkich trzęsień na podstawie zbioru danych.
 all_eq_data = all_eq_data['features']
 
+# Wyodrębnienie danych: siła trzęsienia oraz miejsce.
 mags, lons, lats = [], [], []
 for eq_dict in all_eq_data:
     mag = eq_dict['properties']['mag']
@@ -21,5 +22,10 @@ for eq_dict in all_eq_data:
     lats.append(lat)
 
 title = 'Trzęsienia ziemi na świecie.'
-fig = px.scatter_geo(lat=lats, lon=lons, title=title)
+fig = px.scatter_geo(lat=lats, lon=lons, size=mags, title=title,
+                     color=mags,
+                     color_continuous_scale='Viridis',
+                     labels={'color':'Siła'},
+                     projection='natural earth',
+                     )
 fig.show()
